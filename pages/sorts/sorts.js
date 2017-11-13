475203ddf2f0d10a60f92d35faa8bdea92a2ebc0
marspalLiu <1098846196@qq.com>
2017-11-13 12:14:49 +0800

    刘鑫提交

--- pages/sorts/sorts.js
+++ pages/sorts/sorts.js
@@ -1,4 +1,5 @@
 // pages/sorts/sorts.js
+var app = getApp()
 Page({
 
   /**
@@ -12,7 +13,27 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+      var that = this
+      var url = ('https://' + app.globalData.apiUrl + '?m=home&c=Api&a=getSorts').replace(/\s+/g, "")
+      wx.request({
+          url: url,
+          method: "GET",
+          dataType: "json",
+          success: function (res) {
+              if (res.data == "none") {
+                  wx.showToast({
+                      title: '暂无分类',
+                      image: '../../images/warning.png',
+                      duration: 2000
+                  })
+              } else {
+                  that.setData({
+                      sortsIDArray: res.data["ID"],
+                      sortsNameArray: res.data["sort_name"]
+                  })
+              }
+          }
+      })
   },
 
   /**
